deca53927806d67fd3e0de2c4774adfdb7774e86
roy khater <jerjy.souss@outlook.com>
2019-10-02 13:12:36 +0300

    modified the excscript to include rashadd's name

--- excScript.sh
+++ excScript.sh
@@ -1,13 +1,13 @@
 # excScript
-# replace Name with your name 
+# replace rashadd with your rashadd 
 
 n=1
 for N in 0.1 1 50 
 do
-echo "Creating Name$n folder with N=$N"
+echo "Creating rashadd$n folder with N=$N"
 
-mkdir Name${n}
-cd Name${n}
+mkdir rashadd${n}
+cd rashadd${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
