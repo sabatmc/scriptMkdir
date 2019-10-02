07e4420443134cebaec397decdffc93f0baa0768
Amer Moussa <amer.moussa@std.balamand.edu.lb>
2019-10-02 12:57:26 +0300

    Added Amer's version

--- excScript1.sh
+++ excScript1.sh
@@ -4,14 +4,20 @@
 n=1
 for N in 0.1 1 50 
 do
-echo "Creating Amer$n folder with N=$N"
+m=1
+for M in 100 200 300
+do
+echo "Creating Amer$n_$m folder with N=$N and M=$M"
 
-mkdir Amer${n}
-cd Amer${n}
+mkdir Amer${n}_{m}
+cd Amer${n}_${m}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
-sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
+sed -e "s/xxxx/${N}/" -e "s/yyy/${M}/" inputOrig.txt > input.txt
 #here you can call an executable that will run your simulation using the input parameter
 cd ../
+m=$(( $m + 1 ))
+done
 n=$(( $n + 1 ))
 done
+
