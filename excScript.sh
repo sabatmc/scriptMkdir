d4778dcc58d71e2ed7f7706fcc0cc6ec4b76c374
mustafachaar <mustafa_abdulkareem>
2019-10-30 17:39:32 +0200

    first commit

--- excScript.sh
+++ excScript.sh
@@ -1,13 +1,13 @@
 # excScript
-# replace Name with your name 
+# replace Mustafa with your name 
 
 n=1
 for N in 0.1 1 50 
 do
-echo "Creating Name$n folder with N=$N"
+echo "Creating Mustafa$n folder with N=$N"
 
-mkdir Name${n}
-cd Name${n}
+mkdir Mustafa${n}
+cd Mustafa${n}
 cp ../inputOrig.txt ./
 cp ../input.txt ./
 sed -e "s/xxxx/${N}/" inputOrig.txt > input.txt
